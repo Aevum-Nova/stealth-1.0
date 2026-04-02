325b2a1f6a84b24a1facc49f7de44206e643779e
Arogya Dhakal <arogyadhakal@gmail.com>
2026-04-01 22:26:17 -0400

    fixed a bug

--- connector/src/components/ui/button.tsx
+++ connector/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         ghost: "text-[#707070] hover:text-[#161616] hover:bg-black/[0.04]",
         outline:
           "border border-[rgba(28,25,23,0.14)] bg-transparent text-[#161616] hover:bg-[#f6f5f2]",
-        "dark-primary": "bg-white text-[#141414] hover:bg-white/90",
+        "dark-primary": "bg-[#ffffff] text-[#141414] hover:bg-[#f0f0f0]",
         "dark-secondary":
           "border border-white/20 text-white bg-white/[0.02] hover:bg-white/10",
         link: "text-[#3564e8] underline-offset-4 hover:underline p-0 h-auto",
